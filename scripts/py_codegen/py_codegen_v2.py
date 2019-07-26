3d4014afd90aad32f58cc0a298a91c4c66880b69
Huawei Yang <yanghuawei@hujiang.com>
2019-07-26 11:40:28 +0800

    os.path.sep misused for url>>'/'

--- scripts/py_codegen/py_codegen_v2.py
+++ scripts/py_codegen/py_codegen_v2.py
@@ -159,7 +159,7 @@ class PyCodeGen(object):
 
     def get_path_name(self, path):
         path = path.replace('-', '_')  # resolve /by-key/{key}
-        parts = path.split(os.path.sep)
+        parts = path.split('/')
         if re.match('^v[1-9]+$', parts[1]):
             return ('_'.join(parts[2:]).replace('{', '').replace('}', '') + '_' + parts[1]).lower()
         else:
